6f8022662619abec19d7d5d8424b483d2f4bae48
anjali0626 <a9sharma@scu.edu>
2017-09-19 19:02:10 -0700

    Render userInput.html

--- client/app.js
+++ client/app.js
@@ -1,4 +1,8 @@
 angular.module('to-go-app', [])
+.controller('AppCtrl', function() {
+  this.username = 'Anjali';
+})
 .component('app', {
+  controller:'AppCtrl',
   templateUrl: 'templates/app.html'
 });
